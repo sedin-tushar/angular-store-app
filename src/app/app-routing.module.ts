0596347cf743a74d12ab70a7322441cffb6aea89
sedin-tushar <tushar@sedintechnologies.com>
2023-08-25 13:13:00 +0530

    store app products header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home',
+  component: HomeComponent
+},{
+   path: '', redirectTo: 'home', pathMatch: 'full'
+},{path: 'cart',
+  component: CartComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
