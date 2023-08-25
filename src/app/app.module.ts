439c79e7860fbe72604f819aaf0a846a224b8758
sedin-tushar <tushar@sedintechnologies.com>
2023-08-25 16:17:28 +0530

    Product Boxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 import { FiltersComponent } from './pages/home/components/filters/filters.component';
+import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { FiltersComponent } from './pages/home/components/filters/filters.compon
     HomeComponent,
     CartComponent,
     ProductsHeaderComponent,
-    FiltersComponent
+    FiltersComponent,
+    ProductBoxComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-
+const ROWS_HEIGHT: { [id:number]: number} = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  cols=0;
+  cols=1;
+  rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
   onColumnsCountChange(colsNum: number): void{
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
   onShowCategory(newCategory: string): void{
