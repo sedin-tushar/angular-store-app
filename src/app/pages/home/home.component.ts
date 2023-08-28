99e48df5002129ec51b9de4198f64e672790ed30
sedin-tushar <tushar@sedintechnologies.com>
2023-08-28 12:25:13 +0530

    Fullwidth Mode

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,7 +5,7 @@ const ROWS_HEIGHT: { [id:number]: number} = { 1: 400, 3: 335, 4: 350 };
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  cols=1;
+  cols=3;
   rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
   onColumnsCountChange(colsNum: number): void{
