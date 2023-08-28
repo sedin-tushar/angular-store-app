036465ab78d92d2aa39d25031d81ac2436f3bd29
sedin-tushar <tushar@sedintechnologies.com>
2023-08-29 01:41:46 +0530

    Cart Services To Add Product To Cart

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,11 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
   templateUrl: './product-box.component.html',
 })
 export class ProductBoxComponent {
-@Input() fullWidthMode = false;
+  @Input() fullWidthMode = false;
+  product: Product | undefined={
+    id: 1,
+    title: 'Adidas',
+    price: 1900,
+    category: 'Shoes',
+    description: 'Sport Shoes',
+    image: 'https://via.placeholder.com/150'
+  };
+  @Output() addToCart = new EventEmitter();
 
-
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 const ROWS_HEIGHT: { [id:number]: number} = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   cols=3;
   rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
+
+  constructor(private cartService: CartService){}
+
+  ngOnInit(): void {
+
+  }
+
   onColumnsCountChange(colsNum: number): void{
     this.cols = colsNum;
     this.rowHeight = ROWS_HEIGHT[this.cols];
@@ -15,5 +24,15 @@ export class HomeComponent {
 
   onShowCategory(newCategory: string): void{
     this.category = newCategory;
+  }
+
+  onAddToCart(product: Product): void{
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id
+    });
   }
 }
