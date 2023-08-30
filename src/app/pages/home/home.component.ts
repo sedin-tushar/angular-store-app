de295258dfe49b215e64152291919116ce9a81e3
sedin-tushar <tushar@sedintechnologies.com>
2023-08-30 16:50:54 +0530

    add categories fakeAPI

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -30,7 +30,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   getProducts(): void {
-    this.ProductsSubscription = this.storeService.getAllProducts(this.count, this.sort).subscribe((_products) => {
+    this.ProductsSubscription = this.storeService.getAllProducts(this.count, this.sort, this.category).subscribe((_products) => {
       this.products = _products;
     });
   }
@@ -42,6 +42,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   onShowCategory(newCategory: string): void{
     this.category = newCategory;
+    this.getProducts();
   }
 
   onAddToCart(product: Product): void{
